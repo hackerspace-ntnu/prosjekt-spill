7f617de919745bc89497513ec11d221ff90f214a
chrisjk <chrisjk@stud.ntnu.no>
2024-10-05 13:05:19 +0200

    docs: ad comments in ItemSpawner.cs
    refs: #3

--- scripts/Items/ItemSpawner.cs
+++ scripts/Items/ItemSpawner.cs
@@ -13,6 +13,7 @@ public partial class ItemSpawner : Node3D
 
 			if (item != null && item.itemModel != null)
 			{
+				// Spawn instance of testitem
 				Node3D itemInstance = (Node3D)item.itemModel.Instantiate();
 				itemInstance.GlobalTransform = new Transform3D(Basis.Identity, new Vector3(0, 1, 0));
 				AddChild(itemInstance);
