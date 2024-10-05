42ac363be4a52ae47182c71a247becb4658e109b
crazyb1336 <chrisbraat5@gmail.com>
2024-10-05 12:49:44 +0200

    refactor: refactor the abilitysystem to take in main abilities

--- scripts/player/Ability.cs
+++ scripts/player/Ability.cs
@@ -2,17 +2,23 @@ using Godot;
 
 public partial class Ability : Resource
 {
-    [Export]
-    public Texture2D icon { get; set; }
+	[Export]
+	public Texture2D icon { get; set; }
 
-    [Export]
-    public Animation playerAnimation { get; set; }
+	[Export]
+	public Animation playerAnimation { get; set; }
 
-    public delegate void HandleActivate();
-    public event HandleActivate AbilityActivated;
+	public delegate void HandleActivate();
+	public event HandleActivate AbilityActivated;
+	public event HandleActivate AbilityDeactivated;
 
-    public void Activate()
-    {
-        AbilityActivated?.Invoke();
-    }
+	public void ActivatePressed()
+	{
+		AbilityActivated?.Invoke();;
+	}
+
+	public void ActivateReleased()
+	{
+		AbilityDeactivated?.Invoke();
+	}
 }
