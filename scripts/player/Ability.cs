751d0078316aa4173f351a05c9f8958f3b65f605
Christopher <108337170+CraZyB1336@users.noreply.github.com>
2024-10-19 12:06:25 +0200

    Create ability system (#11)
    * Finish basic movement and camera controls
    
    * Add escape mode for mouse
    
    * Add ability system. Add abilities for soldier class
    
    * Add bullet spread to SoldierPrimary ability
    
    * Create gun model for soldier primary ability
    
    * refactor: refactor the abilitysystem to take in main abilities
    
    * refactor: refactor ability system to have main abilities
    
    * chore: add comments
    
    * chore: add doc to shoot function
    
    * chore: fix comment line
    
    * chore: fix comment line

--- scripts/player/Ability.cs
+++ scripts/player/Ability.cs
@@ -2,21 +2,27 @@ using Godot;
 
 public partial class Ability : Resource
 {
+	// This is the texture for the icon in the UI.
 	[Export]
 	public Texture2D icon { get; set; }
 
+	// This is the animation the player will play when using the ability.
+	// Should we have this here? Can be refactored.
 	[Export]
 	public Animation playerAnimation { get; set; }
 
+	// C# delegate calling all subscribed functions for when ability is activated and deactivated
 	public delegate void HandleActivate();
 	public event HandleActivate AbilityActivated;
 	public event HandleActivate AbilityDeactivated;
 
+	// Calls all subscribed delegates.
 	public void ActivatePressed()
 	{
-		AbilityActivated?.Invoke();;
+		AbilityActivated?.Invoke();
 	}
 
+	// Calls all subscribed delegates.
 	public void ActivateReleased()
 	{
 		AbilityDeactivated?.Invoke();
--- scripts/player/AbilityHandler.cs
+++ scripts/player/AbilityHandler.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class AbilityHandler : Node3D
 {
+	// This is the resources for the abilities. AbilityHandler will keep track of these.
 	[ExportGroup("Character Abilities")]
 	[Export]
 	public Ability primaryAbility { get; private set; }
@@ -19,10 +20,13 @@ public partial class AbilityHandler : Node3D
 	[Export]
 	public Ability ultimateAbility { get; private set; }
 
+	// These are the main abilities, i.e each class can only have one of these at a time.
+	// In the inspector, put the method name as well as the nodepath in their designated areas.
 	[ExportGroup("Main abilities")]
 	[ExportSubgroup("Primary Main")]
 	[Export]
 	private NodePath primaryNodePath;
+	private Node3D primaryNode;
 	[Export]
 	private string primaryActivateMethodName = "TargetMethod";
 	[Export]
@@ -31,6 +35,7 @@ public partial class AbilityHandler : Node3D
 	[ExportSubgroup("Secondary Main")]
 	[Export]
 	private NodePath secondaryNodePath;
+	private Node3D secondaryNode;
 	[Export]
 	private string secondaryActivateMethodName = "TargetMethod";
 	[Export]
@@ -39,6 +44,7 @@ public partial class AbilityHandler : Node3D
 	[ExportSubgroup("UtilityOne Main")]
 	[Export]
 	private NodePath utilityOneNodePath;
+	private Node3D utilityOneNode;
 	[Export]
 	private string utilityOneActivateMethodName = "TargetMethod";
 	[Export]
@@ -48,6 +54,7 @@ public partial class AbilityHandler : Node3D
 	[ExportSubgroup("UtilityTwo Main")]
 	[Export]
 	private NodePath utilityTwoNodePath;
+	private Node3D utilityTwoNode;
 	[Export]
 	private string utilityTwoActivateMethodName = "TargetMethod";
 	[Export]
@@ -56,61 +63,91 @@ public partial class AbilityHandler : Node3D
 	[ExportSubgroup("Ultimate Main")]
 	[Export]
 	private NodePath ultimateNodePath;
+	private Node3D ultimateNode;
 	[Export]
 	private string ultimateActivateMethodName = "TargetMethod";
 	[Export]
 	private string ultimateDeactivateMethodName = "TargetMethod";
 
+	public override void _Ready()
+	{
+		if (primaryNodePath != null)
+			primaryNode = GetNode<Node3D>(primaryNodePath);
+
+		if (secondaryNodePath != null)
+			secondaryNode = GetNode<Node3D>(secondaryNodePath);
+
+		if (utilityOneNodePath != null)
+			utilityOneNode = GetNode<Node3D>(utilityOneNodePath);
+
+		if (utilityTwoNodePath != null)
+			utilityTwoNode = GetNode<Node3D>(utilityTwoNodePath);
+
+		if (ultimateNodePath != null)
+			ultimateNode = GetNode<Node3D>(ultimateNodePath);
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
+		// Calls the respective abilities based on the inputmap.
 		if (Input.IsActionPressed("Primary"))
 		{
 			primaryAbility.ActivatePressed();
+			primaryNode?.Call(primaryActivateMethodName);
 		}
 		else if (Input.IsActionJustReleased("Primary"))
 		{
 			// Unsure if this is the only else case, thats why else if.
 			primaryAbility.ActivateReleased();
+			primaryNode?.Call(primaryDeactivateMethodName);
 		}
 
 		if (Input.IsActionPressed("Secondary"))
 		{
 			secondaryAbility.ActivatePressed();
+			secondaryNode?.Call(secondaryActivateMethodName);
 		}
 		else if (Input.IsActionJustReleased("Secondary"))
 		{
 			// Unsure if this is the only else case, thats why else if.
 			secondaryAbility.ActivateReleased();
+			secondaryNode?.Call(secondaryDeactivateMethodName);
 		}
 
 		if (Input.IsActionPressed("UtilityOne"))
 		{
 			utilityOneAbility.ActivatePressed();
+			utilityOneNode?.Call(utilityOneActivateMethodName);
 		}
 		else if (Input.IsActionJustReleased("UtilityOne"))
 		{
 			// Unsure if this is the only else case, thats why else if.
 			utilityOneAbility.ActivateReleased();
+			utilityOneNode?.Call(utilityOneDeactivateMethodName);
 		}
 
 		if (Input.IsActionPressed("UtilityTwo"))
 		{
 			utilityOneAbility.ActivatePressed();
+			utilityTwoNode?.Call(utilityTwoActivateMethodName);
 		}
 		else if (Input.IsActionJustReleased("UtilityTwo"))
 		{
 			// Unsure if this is the only else case, thats why else if.
 			utilityTwoAbility.ActivateReleased();
+			utilityTwoNode?.Call(utilityTwoDeactivateMethodName);
 		}
 
 		if (Input.IsActionPressed("Ultimate"))
 		{
 			ultimateAbility.ActivatePressed();
+			ultimateNode?.Call(ultimateActivateMethodName);
 		}
 		else if (Input.IsActionJustReleased("Ultimate"))
 		{
 			// Unsure if this is the only else case, thats why else if.
 			ultimateAbility.ActivateReleased();
+			ultimateNode?.Call(ultimateDeactivateMethodName);
 		}
 	}
 }
