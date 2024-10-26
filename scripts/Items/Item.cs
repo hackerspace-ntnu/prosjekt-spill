d83636858a4b4735ac908efcbf1b64d02a2e7c0a
chrisjk <chrisjk@stud.ntnu.no>
2024-10-26 12:54:35 +0200

    feat: :sparkles: add simple item resource template
    refs: #3

--- scripts/Items/Item.cs
+++ scripts/Items/Item.cs
@@ -1,11 +1,41 @@
 using Godot;
+using Godot.Collections;
 
 [GlobalClass]
+[Tool]
 public partial class Item : Resource
 {
+	[ExportGroup("Item properties")]
 	[Export] public string itemName { set; get; }
 	[Export(PropertyHint.Enum, "Common,Rare,Legendary")]
+
 	public string itemGrade { set; get; }
+	
+	[ExportGroup("Item visuals")]
 	[Export] public Texture itemImage { set; get; }
 	[Export] public PackedScene itemModel { set; get; }
+	
+	// Conditional rendering of inspector elements is difficult. Will see if i can convert to costum plugin later
+	[ExportGroup("Class Specificity")]
+	private bool _classSpecific;
+	[Export]
+	public bool ClassSpecific
+	{
+		get => _classSpecific;
+		set
+		{
+			_classSpecific = value;
+			NotifyPropertyListChanged();
+		}
+	}
+
+	[Export(PropertyHint.Enum, "Soldier,Melee,Mage,Engineer")]
+	public string ItemClass { set; get; }
+
+	public override void _ValidateProperty(Dictionary property)
+	{
+		if(property["name"].AsStringName() == PropertyName.ItemClass && !ClassSpecific) {
+			property["usage"] = (int)property["usage"].As<PropertyUsageFlags>() | (int)PropertyUsageFlags.ReadOnly;
+		}
+	}
 }
--- scripts/Items/ItemSpawner.cs
+++ scripts/Items/ItemSpawner.cs
@@ -8,11 +8,14 @@ public partial class ItemSpawner : Node3D
 	public override void _Ready()
 	{
 		if (itemResource != null)
-		{
+		{		
 			Item item = itemResource as Item;
 
 			if (item != null && item.itemModel != null)
 			{
+				GD.Print("Item Resource Name: ", item.itemName);
+				item.itemName = "Health Potion";
+				GD.Print("Item Resource Name: ", item.itemName);
 				// Spawn instance of testitem
 				Node3D itemInstance = (Node3D)item.itemModel.Instantiate();
 				itemInstance.GlobalTransform = new Transform3D(Basis.Identity, new Vector3(0, 1, 0));
